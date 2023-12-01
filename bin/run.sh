d01f27c2e0f6f9753c952736deacda5ccf0069b1
Vincent C. Mader <vincent.mader@icloud.com>
2023-12-01 02:01:37 +0100

    FEAT Implement `lg` (CLI-based logging tool)

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+a="$0"
+b="$(realpath $a)"
+c="$(dirname $b)"
+
 # Define path to python executable (in virtual environment).
-  PYTHON3="../.venv/bin/python3"
+  PYTHON3="$c/../.venv/bin/python3"
 
 # Execute main python entrypoint.
-  cd ../src && $PYTHON3 ./main.py
+  cd "$c/../src" && $PYTHON3 ./main.py
--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello, world!")
+from datetime import datetime as dt
+from pathlib import Path
+import os
+
+from termcolor import colored
+
+
+PATH_TO_LOGS = Path("..", "logs")
+COLOR = "green"
 
 
 if __name__ == "__main__":
-    main()
+
+    date = dt.now().strftime("%Y-%m-%d")
+    time = dt.now().strftime("%H:%M:%S")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    welcome = f"Date: {date}\nTime:   {time}\nLog: "
+
+    lines = [input(colored(welcome, COLOR))]
+    while True:
+        line = input(">> ")
+        if line == ":wq":
+            break
+        lines.append(line)
+
+    path = Path(PATH_TO_LOGS, f"{date}.org")
+    os.makedirs(PATH_TO_LOGS, exist_ok=True)
+    with open(path, 'a') as fp:
+        content = f"\n\n* {date} {time}\n\n" + '\n'.join(lines)
+        fp.write(content)
+
+    n = len(content)
+    print(colored(f"Wrote {n} characters to \"{path}\"", COLOR))
