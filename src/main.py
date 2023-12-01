3043ff9185a43add3f5b0c33ac2f1b63761f7fc1
Vincent C. Mader <vincent.mader@icloud.com>
2023-12-01 02:01:37 +0100

    FEAT Implement `lg` (CLI-based logging tool)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello, world!")
+from datetime import datetime as dt
+from pathlib import Path
+import os, sys
+
+from termcolor import colored
+
+
+PATH_TO_LOGS = Path("..", "logs")
+COLOR = "green"
 
 
 if __name__ == "__main__":
-    main()
+
+    date = dt.now().strftime("%Y-%m-%d")
+    time = dt.now().strftime("%H:%M:%S")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    welcome = f"Date: {date}\nTime:   {time}\nLog: "
+
+    line = input(colored(welcome, COLOR))
+    lines = [line]
+    while True:
+        if line.endswith(":q"):
+            sys.exit()
+        if line.endswith(":wq"):
+            break
+        lines.append(line)
+        line = input(" >>  ")
+
+    path = Path(PATH_TO_LOGS, f"{date}.org")
+    os.makedirs(PATH_TO_LOGS, exist_ok=True)
+    with open(path, 'a') as fp:
+        content = f"\n\n* {date} {time}\n\n" + '\n'.join(lines)
+        fp.write(content)
+
+    n = len(content)
+    print(colored(f"Wrote {n} characters to \"{path}\"", COLOR))
